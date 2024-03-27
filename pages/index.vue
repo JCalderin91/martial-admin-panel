2328a6ae32a104566427fbb621ad517197473697
Jesus Calderin <jesus.c@servicepad.com>
2024-03-27 00:07:40 -0400

    Instalado Vuetify

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Bienvenido a Martial Admin</h1>
+    <v-btn color="primary">Button</v-btn>
+  </div>
+</template>
+
+<style scoped>
+div {
+  text-align: center;
+}
+</style>
