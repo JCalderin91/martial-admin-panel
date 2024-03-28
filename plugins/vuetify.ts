4f34cf136cb6a2d5f421b562504b7d0eb2592a93
Jesus Calderin <jesus.c@servicepad.com>
2024-03-28 09:11:50 -0400

    Theme config

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,12 +1,42 @@
 // import this after install `@mdi/font` package
-import '@mdi/font/css/materialdesignicons.css'
+import "@mdi/font/css/materialdesignicons.css";
 
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
 
 export default defineNuxtPlugin((app) => {
   const vuetify = createVuetify({
-    // ... your configuration
-  })
-  app.vueApp.use(vuetify)
-})
+    theme: {
+      defaultTheme: "custom",
+      themes: {
+        custom: {
+          colors: {
+            primary: "#0113ef",
+            secondary: "#f6cc1e",
+          },
+        },
+      },
+    },
+    defaults: {
+      VTextField: {
+        variant: "outlined",
+        density: "compact",
+      },
+      VSelect: {
+        variant: "outlined",
+        density: "compact",
+      },
+      VBtn: {
+        size: "small",
+      },
+      VCard: {
+        VCardActions: {
+          VBtn: {
+            variant: "elevated",
+          },
+        },
+      },
+    },
+  });
+  app.vueApp.use(vuetify);
+});
